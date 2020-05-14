dd46bd70709f3204b737a5605fb0cf260f871bee
RosaitaW <55056093+RosaitaW@users.noreply.github.com>
2020-05-14 13:09:48 -0400

    Added basic UI and movements

---
+++ CodeLab1-FinalProject/Assets/Scripts/ButtonClick.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonClick : MonoBehaviour
+{
+    public Transform targetCube;
+    public float xPosition;
+    public float yPosition;
+    public float zPosition;
+    public GameObject Up;
+    public GameObject Down;
+    public GameObject Left;
+    public GameObject Right;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    public void LookUp()
+    {
+        targetCube.transform.Translate(0f, yPosition, 0f);
+        //targetCube.transform.position = new Vector3(Cx, Cy + yPosition, Cz);
+        if (targetCube.transform.position.y == 0)
+        {
+            Up.SetActive(true);
+            Left.SetActive(true);
+            Right.SetActive(true);
+            Down.SetActive(true);
+        }
+        else
+        {
+            Up.SetActive(false);
+            Left.SetActive(false);
+            Right.SetActive(false);
+            Down.SetActive(true);
+        }
+    }
+    public void LookDown()
+    {
+        targetCube.transform.Translate(0f, -yPosition, 0f);
+        //targetCube.transform.position = new Vector3(transform.position.x, transform.position.y - yPosition, transform.position.z);
+        if (targetCube.transform.position.y == 0)
+        {
+            Up.SetActive(true);
+            Left.SetActive(true);
+            Right.SetActive(true);
+            Down.SetActive(true);
+        }
+        else
+        {
+            Up.SetActive(true);
+            Left.SetActive(false);
+            Right.SetActive(false);
+            Down.SetActive(false);
+        }   
+    }
+    public void LookLeft()
+    {
+        if (targetCube.transform.position.x == -15 && targetCube.transform.position.z==0)
+        {
+            targetCube.transform.Translate(xPosition, 0f, -zPosition);
+        }
+        else if(targetCube.transform.position.x == 0 && targetCube.transform.position.z == 15)
+        {
+            targetCube.transform.Translate(-xPosition, 0f, -zPosition);
+        }
+        else if (targetCube.transform.position.x == 0 && targetCube.transform.position.z == -15)
+        {
+            targetCube.transform.Translate(xPosition, 0f, zPosition);
+        }
+        else
+        {
+            targetCube.transform.Translate(-xPosition, 0f, zPosition);
+        }
+        Up.SetActive(true);
+        Left.SetActive(true);
+        Right.SetActive(true);
+        Down.SetActive(true);
+    }
+    public void LookRight()
+    {
+        if (targetCube.transform.position.x == -15 && targetCube.transform.position.z == 0)
+        {
+            targetCube.transform.Translate(xPosition, 0f, zPosition);
+        }
+        else if (targetCube.transform.position.x == 0 && targetCube.transform.position.z == 15)
+        {
+            targetCube.transform.Translate(xPosition, 0f, -zPosition);
+        }
+        else if (targetCube.transform.position.x == 0 && targetCube.transform.position.z == -15)
+        {
+            targetCube.transform.Translate(-xPosition, 0f, zPosition);
+        }
+        else
+        {
+            targetCube.transform.Translate(-xPosition, 0f, -zPosition);
+        }
+        Up.SetActive(true);
+        Left.SetActive(true);
+        Right.SetActive(true);
+        Down.SetActive(true);
+    }
+}
---
+++ CodeLab1-FinalProject/Assets/Scripts/CameraLookForward.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLookForward : MonoBehaviour
+{
+    public Transform target;
+    public float timeMod = 0.1f;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null)
+        { 
+            return;//No target, no functions
+        }
+        //Look at the target
+        Vector3 forward = target.position - transform.position;
+        Quaternion targetRotation = Quaternion.LookRotation(forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * timeMod);
+        Debug.DrawLine(transform.position, target.position, Color.red);
+        Debug.DrawRay(transform.position, transform.forward * 5f, Color.blue);
+    }
+}
