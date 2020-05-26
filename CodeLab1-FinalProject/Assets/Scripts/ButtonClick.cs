e6540045cfa9b6e0ffa46e2f800c52be166814ee
RosaitaW <55056093+RosaitaW@users.noreply.github.com>
2020-05-25 23:18:20 -0400

    Updated scripts for the next add-up

--- CodeLab1-FinalProject/Assets/Scripts/ButtonClick.cs
+++ CodeLab1-FinalProject/Assets/Scripts/ButtonClick.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class ButtonClick : MonoBehaviour
 {
+    public Transform mainCamera;
     public Transform targetCube;
-    public float xPosition;
+    public float distance = 15;
+    //public float xPosition;
     public float yPosition;
-    public float zPosition;
+    //public float zPosition;
     public GameObject Up;
     public GameObject Down;
     public GameObject Left;
@@ -15,13 +17,14 @@ public class ButtonClick : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        targetCube.transform.position = new Vector3(mainCamera.transform.position.x, mainCamera.transform.position.y, mainCamera.transform.position.z + distance);
     }
 
     public void LookUp()
     {
         targetCube.transform.Translate(0f, yPosition, 0f);
         //targetCube.transform.position = new Vector3(Cx, Cy + yPosition, Cz);
-        if (targetCube.transform.position.y == 0)
+        if (targetCube.transform.position.y == mainCamera.transform.position.y)
         {
             Up.SetActive(true);
             Left.SetActive(true);
@@ -40,7 +43,7 @@ public class ButtonClick : MonoBehaviour
     {
         targetCube.transform.Translate(0f, -yPosition, 0f);
         //targetCube.transform.position = new Vector3(transform.position.x, transform.position.y - yPosition, transform.position.z);
-        if (targetCube.transform.position.y == 0)
+        if (targetCube.transform.position.y == mainCamera.transform.position.y)
         {
             Up.SetActive(true);
             Left.SetActive(true);
@@ -57,21 +60,21 @@ public class ButtonClick : MonoBehaviour
     }
     public void LookLeft()
     {
-        if (targetCube.transform.position.x == -15 && targetCube.transform.position.z==0)
+        if (targetCube.transform.position.x == mainCamera.transform.position.x- distance && targetCube.transform.position.z== mainCamera.transform.position.z)//left side
         {
-            targetCube.transform.Translate(xPosition, 0f, -zPosition);
+            targetCube.transform.Translate(distance, 0f, -distance);
         }
-        else if(targetCube.transform.position.x == 0 && targetCube.transform.position.z == 15)
+        else if(targetCube.transform.position.x == mainCamera.transform.position.x && targetCube.transform.position.z == mainCamera.transform.position.z+ distance)//front side
         {
-            targetCube.transform.Translate(-xPosition, 0f, -zPosition);
+            targetCube.transform.Translate(-distance, 0f, -distance);
         }
-        else if (targetCube.transform.position.x == 0 && targetCube.transform.position.z == -15)
+        else if (targetCube.transform.position.x == mainCamera.transform.position.x && targetCube.transform.position.z == mainCamera.transform.position.z - distance)//back side
         {
-            targetCube.transform.Translate(xPosition, 0f, zPosition);
+            targetCube.transform.Translate(distance, 0f, distance);
         }
-        else
+        else//right side
         {
-            targetCube.transform.Translate(-xPosition, 0f, zPosition);
+            targetCube.transform.Translate(-distance, 0f, distance);
         }
         Up.SetActive(true);
         Left.SetActive(true);
@@ -80,21 +83,21 @@ public class ButtonClick : MonoBehaviour
     }
     public void LookRight()
     {
-        if (targetCube.transform.position.x == -15 && targetCube.transform.position.z == 0)
+        if (targetCube.transform.position.x == mainCamera.transform.position.x - distance && targetCube.transform.position.z == mainCamera.transform.position.z)
         {
-            targetCube.transform.Translate(xPosition, 0f, zPosition);
+            targetCube.transform.Translate(distance, 0f, distance);
         }
-        else if (targetCube.transform.position.x == 0 && targetCube.transform.position.z == 15)
+        else if (targetCube.transform.position.x == mainCamera.transform.position.x && targetCube.transform.position.z == mainCamera.transform.position.z+ distance)
         {
-            targetCube.transform.Translate(xPosition, 0f, -zPosition);
+            targetCube.transform.Translate(distance, 0f, -distance);
         }
-        else if (targetCube.transform.position.x == 0 && targetCube.transform.position.z == -15)
+        else if (targetCube.transform.position.x == mainCamera.transform.position.x && targetCube.transform.position.z == mainCamera.transform.position.z - distance)
         {
-            targetCube.transform.Translate(-xPosition, 0f, zPosition);
+            targetCube.transform.Translate(-distance, 0f, distance);
         }
         else
         {
-            targetCube.transform.Translate(-xPosition, 0f, -zPosition);
+            targetCube.transform.Translate(-distance, 0f, -distance);
         }
         Up.SetActive(true);
         Left.SetActive(true);
--- CodeLab1-FinalProject/Assets/Scripts/ClickDoor.cs
+++ CodeLab1-FinalProject/Assets/Scripts/ClickDoor.cs
@@ -7,6 +7,7 @@ public class ClickDoor : MonoBehaviour
 {
     public GameObject keyManager;
     public bool isUnlocked = false;
+    public bool isOpen = false;
     public Text What2Do;
     public GameObject ExitButton;
     // Start is called before the first frame update
@@ -18,9 +19,10 @@ public class ClickDoor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (keyManager.GetComponent<KeyManager>().CountNumber == 0)
+        if (keyManager.GetComponent<KeyManager>().CountNumber == 0 && !isOpen)
         {
             isUnlocked = true;
+            What2Do.text = "I should check the door now";
         }
     }
     private void OnMouseDown()
@@ -32,6 +34,7 @@ public class ClickDoor : MonoBehaviour
             mousePos = Camera.main.ScreenToViewportPoint(mousePos);
             if (isUnlocked)
             {
+                isOpen = true;
                 gameObject.transform.Rotate(Vector3.up, 90f);
                 What2Do.text = "The door is opened";
                 ExitButton.SetActive(true);
